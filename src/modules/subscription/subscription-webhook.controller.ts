529839ac4b4b9cbd6a100af49218b9e8ab154b19
anirban00537 <anirban00537@gmail.com>
2024-10-19 17:06:31 +0600

    added

--- src/modules/subscription/subscription-webhook.controller.ts
+++ src/modules/subscription/subscription-webhook.controller.ts
@@ -23,10 +23,7 @@ export class SubscriptionWebhookController {
     @Headers('X-Signature') signature: string,
   ) {
     try {
-      console.log('Webhook received', {
-        eventType,
-        signature,
-      });
+      console.log('Webhook received', req);
       if (!eventType || !signature) {
         throw new HttpException('Invalid signature', HttpStatus.UNAUTHORIZED);
       }
